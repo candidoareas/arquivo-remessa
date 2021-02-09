71508bd35cf536cd074c59b916b4c7ee9cba86fa
Cândido Antonio Freitas Areas <candido.areas@gmail.com>
2021-02-08 21:35:56 -0400

    Enum referente à opção de representar comma.

---
+++ src/main/java/com/candidoareas/arquivo/remessa/annotations/enums/RemessaComma.java
@@ -0,0 +1,27 @@
+/*
+ * Código desenvolvido por candido.areas
+ * Para maiores informações, candido.areas@gmail.com
+ */
+package com.candidoareas.arquivo.remessa.annotations.enums;
+
+
+/**
+ * Enum que representa se as commas serão exibidas ou não.
+ *
+ * @author   candido.areas
+ * @version  1
+ * @since    1.0
+ */
+public enum RemessaComma
+{
+
+    /** Padrão */
+    WITHOUT_COMMA,
+
+    /** Comma incluso no final da linha */
+    FULL_COMMA,
+
+    /** Comma não inserido no final da linha */
+    SINGLE_COMMA
+
+}
