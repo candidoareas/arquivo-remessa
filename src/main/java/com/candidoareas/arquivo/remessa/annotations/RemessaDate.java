6958facfbacce59328b65ed26c3b4d75ba6f0909
Cândido Antonio Freitas Areas <candido.areas@gmail.com>
2021-02-08 21:33:53 -0400

    Adição de elementos de saída comma.

--- src/main/java/com/candidoareas/arquivo/remessa/annotations/RemessaDate.java
+++ src/main/java/com/candidoareas/arquivo/remessa/annotations/RemessaDate.java
@@ -6,6 +6,7 @@
  */
 package com.candidoareas.arquivo.remessa.annotations;
 
+import com.candidoareas.arquivo.remessa.annotations.enums.RemessaComma;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -26,4 +27,8 @@ public @interface RemessaDate
     String pattern();
 
     int position();
+    
+    RemessaComma applyComma() default RemessaComma.WITHOUT_COMMA;
+    
+    String commaType() default ";";
 }
--- src/main/java/com/candidoareas/arquivo/remessa/annotations/RemessaText.java
+++ src/main/java/com/candidoareas/arquivo/remessa/annotations/RemessaText.java
@@ -1,10 +1,16 @@
 /*
+ * Código desenvolvido por candido.areas
+ * Para maiores informações, candido.areas@gmail.com
+ */
+/*
  * Desenvolvido por Cândido Areas
  * http://www.candidoareas.com
  *
  * Todos os direitos reservados
  */
 package com.candidoareas.arquivo.remessa.annotations;
+
+import com.candidoareas.arquivo.remessa.annotations.enums.RemessaComma;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -26,4 +32,8 @@ public @interface RemessaText
     int length();
 
     int position();
+
+    RemessaComma applyComma() default RemessaComma.WITHOUT_COMMA;
+
+    String commaType() default ";";
 }
